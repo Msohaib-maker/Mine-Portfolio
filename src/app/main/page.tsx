d325eab661d88bfda6eda02c63c12db65242332b
Msohaib-maker <afzalsohaib59@gmail.com>
2024-11-03 04:04:21 +0500

    Added personal picture and description

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,5 +1,17 @@
 export default function Main(){
     return(
-        <div>Main Page</div>
+        <div className="flex flex-col items-center">
+            <h1>Main Page</h1>
+            <img
+                src="/images/avatar.jpg" // Ensure the path is correct
+                className="h-80 w-80 rounded-full" // Change h-40 and w-40 to your desired size
+                alt="Avatar"
+                style={{ borderWidth: '10px', borderColor: '#800080', borderStyle: 'solid' }}
+            />
+            <div className="w-120">
+            <h2>I am hardcore and 🚀 passionate software engineer who has experience in HTML, CSS, JavaScript, React.JS, Next.JS, Unity, OpenGL, C#, C++, Java. I believe in following the best pratices in the software industry. </h2>
+            </div>
+            
+        </div>
     );
 }
