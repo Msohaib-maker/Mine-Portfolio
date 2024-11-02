016912dd5207df9ca80ab6a3065eb0742534d6f3
Msohaib-maker <afzalsohaib59@gmail.com>
2024-11-03 04:32:08 +0500

    Main page update

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link"
+
 export default function Main(){
     return(
         <div className="flex flex-col items-center">
@@ -9,9 +11,35 @@ export default function Main(){
                 style={{ borderWidth: '10px', borderColor: '#800080', borderStyle: 'solid' }}
             />
             <div className="w-120">
+                <h1>Who am I?</h1>
             <h2>I am hardcore and 🚀 passionate software engineer who has experience in HTML, CSS, JavaScript, React.JS, Next.JS, Unity, OpenGL, C#, C++, Java. I believe in following the best pratices in the software industry. </h2>
             </div>
+            <Link href="/portfolio">
+                <button>Check my portfolio</button>
+            </Link>
             
+            <h1>
+                JavaScript 8/10,
+                HTML 8/10,
+                CSS 8/10,
+                React JS 8/10,
+                Next JS 8/10,
+                C++ 6/10,
+                C# 7/10
+                OpenGL 3/10,
+                Unity 7/10,
+                Java 8/10,
+                DevOps 6/10,
+                Git 7/10,
+                Figma 8/10,
+                Node 6/10
+            </h1>
+            
+            <div>
+                <img src="/images/avatar.jpg"/>
+                <img src="/images/avatar.jpg"/>
+            </div>
+
         </div>
     );
 }
